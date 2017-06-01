7f639d6bd6071ef841c1cdee6af580ce4558e591
Scott Thiessen <scott@varyd.io>
2017-06-01 10:43:20 -0700

    Add missing default value functionality to config
    The val method of the ConfigModel is meant to have this default value
    second argument (in case the config field doesn’t exist), and somehow
    never made it in (despite being expected in a number of places in the
    code).

--- App/resources/scripts/artic/models/AppModel.js
+++ App/resources/scripts/artic/models/AppModel.js
@@ -367,7 +367,7 @@ function AppModel() {
 
 	function isInPriorityGallery(artwork) {
 
-		var ids	= _config.has("priorityGalleryIds") ? _config.val("priorityGalleryIds") : [ ];
+		var ids	= _config.val("priorityGalleryIds", [ ]);
 
 		for (var i = 0; i < ids.length; i++) {
 			if (artwork.galleryId == ids[i]) {
