53ed3d0dec8f4be170a6d8f206f769a1a95b5dca
nikhiltri <ntrivedi@artic.edu>
2023-02-24 18:19:22 -0600

    Update download bash script to backup assets folder and delete old backups [IP-194]

--- AssetDownloader/download.sh
+++ AssetDownloader/download.sh
@@ -1,18 +1,25 @@
 
-DIR_DOWNLODER="$( cd "$(dirname $0)" ; pwd -P )"
+DIR_DOWNLOADER="$( cd "$(dirname $0)" ; pwd -P )"
+cd $DIR_DOWNLOADER
 
 echo " We are currently in the '$DIR_DOWNLOADER' path..."
 
-
-DIR_ROOT="$( cd "$(dirname $0)" ; pwd -P )"
-
+## Back up assers directory
 cd ..
 cd App
 
-rm -R assets
+mv assets assets_bk_`date +%Y_%m_%d_%H:%M:%S`
 mkdir assets
 
+## Download assets
 cd ..
 cd AssetDownloader
 
 node download.js
+
+## Clean up old assets directories. Leave only the five most recent backups
+cd ..
+while [ $(find App -name assets_bk_* -maxdepth 1 -type d | wc -l) -gt 5 ]
+do
+    find App -name assets_bk_* -maxdepth 1 -type d | sort -n | head -n 1 | xargs rm -rf
+done
