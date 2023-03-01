6073a64775f5e2d6d8cd75ca69f8ba1a1a50a817
nikhiltri <ntrivedi@artic.edu>
2023-03-01 09:02:17 -0600

    Refactor download.sh to run all commands from a root directory [IP-194]

--- AssetDownloader/download.sh
+++ AssetDownloader/download.sh
@@ -1,25 +1,18 @@
 
 DIR_DOWNLOADER="$( cd "$(dirname $0)" ; pwd -P )"
-cd $DIR_DOWNLOADER
+JM_ROOT=$DIR_DOWNLOADER/..
 
-echo " We are currently in the '$DIR_DOWNLOADER' path..."
+echo "JourneyMaker root is '$JM_ROOT'"
 
-## Back up assers directory
-cd ..
-cd App
-
-mv assets assets_bk_`date +%Y_%m_%d_%H%M%S`
-mkdir assets
+mv $JM_ROOT/App/assets $JM_ROOT/App/assets_bk_`date +%Y_%m_%d_%H%M%S`
+mkdir $JM_ROOT/App/assets
 
 ## Download assets
-cd ..
-cd AssetDownloader
-
-node download.js
+node $JM_ROOT/AssetDownloader/download.js
 
 ## Clean up old assets directories. Leave only the five most recent backups
 cd ..
-while [ $(find App -name assets_bk_* -maxdepth 1 -type d | wc -l) -gt 5 ]
+while [ $(find $JM_ROOT/App -name assets_bk_* -maxdepth 1 -type d | wc -l) -gt 5 ]
 do
-    find App -name assets_bk_* -maxdepth 1 -type d | sort -n | head -n 1 | xargs rm -rf
+    find $JM_ROOT/App -name assets_bk_* -maxdepth 1 -type d | sort -n | head -n 1 | xargs rm -rf
 done
